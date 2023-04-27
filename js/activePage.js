0de9e3ca15ea1619b3c8b6a5e85d2516eac6b76d
Aklilu Mekonnen <116703083+akezeth@users.noreply.github.com>
2023-04-27 07:55:08 -0700

    Update activePage.js

--- js/activePage.js
+++ js/activePage.js
@@ -1,8 +1,14 @@
-const activePage = window.location.pathname;
+const activePage = window.location.href;
+const githublink = 'https://akezeth.github.io/HTML-CSS-JavaScript-Capstone-Project/';
+
+if (githublink === activePage) {
+  activePage = activePage.concat('index.html')
+}
+
 document.querySelectorAll('nav a').forEach((link) => {
   const linkrefs = link.getAttribute('href');
-  const linkref = linkrefs.substring(1);
-  if (linkref === activePage) {
+  const linkref = linkrefs.substring(2);
+  if (linkref === activePage.substring(githublink.length)) {
     link.classList.add('active');
   }
 });
